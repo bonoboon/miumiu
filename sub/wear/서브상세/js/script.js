65a9989f8adbd9e35bc3be7d7f47ac32b54485b0
unknown105227 <unknown105227@gmail.com>
2022-07-13 16:59:05 +0900

    상품 상세정보 h3 스크립트 태그 추가 및 b-box 오타 수정2 -성승현-

---
+++ sub/wear/서브상세/js/script.js
@@ -0,0 +1,41 @@
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+}
+
+$(document).ready(function() {
+    //explain, option hidden
+    var header = (($("header").offset().top)+ 180);
+    $(window).scroll (function() {
+        var height = $(window).scrollTop();
+        var lastImg = ($(".lastImg").offset().top - header);
+
+        if (height > lastImg) {
+            $(".explain").addClass("on");
+            $(".option").addClass("on");
+        } else {
+            $(".explain").removeClass("on");
+            $(".option").removeClass("on");
+        }
+    });
+
+    // iconHover
+    $('.header-icon > a, .w-b-box > .wishlist-box > a, .w-b-box > .basket-box > a').each(function() {
+        var nowIcon = $(this).find('img');
+        var srcIcon = nowIcon.attr('src');
+        var newIcon = srcIcon.substring(0, srcIcon.lastIndexOf('.'));
+
+        $(this).hover(function() {
+            $(this).find('img').attr('src', newIcon + '_on.' + /[^.]+$/.exec(srcIcon));
+        }, function() {
+            $(this).find('img').attr('src', newIcon + '.' + /[^.]+$/.exec(srcIcon));
+        });
+    });
+
+    //상품상세정보
+    $(function() {
+        $(".detail > p").hide();
+        $(".detail > h3").click(function() {
+            $(".detail > p").slideToggle();
+        });
+    });
+});
